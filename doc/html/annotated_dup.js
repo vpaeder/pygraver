f88bd63d6299113b60057d576ea2a3785b0f7289
Vincent Paeder <vpaeder@users.noreply.github.com>
2022-08-23 09:57:36 +0300

    autogenerated docs #1

---
+++ doc/html/annotated_dup.js
@@ -0,0 +1,51 @@
+var annotated_dup =
+[
+    [ "pybind11", null, [
+      [ "detail", null, [
+        [ "holder_helper< vtkSmartPointer< T > >", "structpybind11_1_1detail_1_1holder__helper_3_01vtk_smart_pointer_3_01_t_01_4_01_4.html", null ],
+        [ "type_caster< Class, enable_if_t< std::is_base_of< vtkObject, Class >::value > >", "structpybind11_1_1detail_1_1type__caster_3_01_class_00_01enable__if__t_3_01std_1_1is__base__of_3b44f15e96beeab99f312b7c88274b39f.html", null ],
+        [ "type_caster< vtkNew< Class > >", "structpybind11_1_1detail_1_1type__caster_3_01vtk_new_3_01_class_01_4_01_4.html", null ],
+        [ "type_caster< vtkSmartPointer< Class > >", "structpybind11_1_1detail_1_1type__caster_3_01vtk_smart_pointer_3_01_class_01_4_01_4.html", null ],
+        [ "vtk_ptr_cast_and_load", "structpybind11_1_1detail_1_1vtk__ptr__cast__and__load.html", null ],
+        [ "vtk_ptr_cast_only", "structpybind11_1_1detail_1_1vtk__ptr__cast__only.html", null ]
+      ] ]
+    ] ],
+    [ "pygraver", null, [
+      [ "render", "namespacepygraver_1_1render.html", [
+        [ "Cylinder", "classpygraver_1_1render_1_1_cylinder.html", "classpygraver_1_1render_1_1_cylinder" ],
+        [ "CylindricalWire", "classpygraver_1_1render_1_1_cylindrical_wire.html", "classpygraver_1_1render_1_1_cylindrical_wire" ],
+        [ "CylindricalWireCollection", "classpygraver_1_1render_1_1_cylindrical_wire_collection.html", "classpygraver_1_1render_1_1_cylindrical_wire_collection" ],
+        [ "Extrusion", "classpygraver_1_1render_1_1_extrusion.html", "classpygraver_1_1render_1_1_extrusion" ],
+        [ "Marker", "classpygraver_1_1render_1_1_marker.html", "classpygraver_1_1render_1_1_marker" ],
+        [ "MarkerCollection", "classpygraver_1_1render_1_1_marker_collection.html", "classpygraver_1_1render_1_1_marker_collection" ],
+        [ "Model", "classpygraver_1_1render_1_1_model.html", "classpygraver_1_1render_1_1_model" ],
+        [ "PyModel", "classpygraver_1_1render_1_1_py_model.html", "classpygraver_1_1render_1_1_py_model" ],
+        [ "PyShape3D", "classpygraver_1_1render_1_1_py_shape3_d.html", "classpygraver_1_1render_1_1_py_shape3_d" ],
+        [ "Shape3D", "classpygraver_1_1render_1_1_shape3_d.html", "classpygraver_1_1render_1_1_shape3_d" ],
+        [ "vtkCustomInteractorStyle", "classpygraver_1_1render_1_1vtk_custom_interactor_style.html", "classpygraver_1_1render_1_1vtk_custom_interactor_style" ],
+        [ "vtkModelCallback", "classpygraver_1_1render_1_1vtk_model_callback.html", "classpygraver_1_1render_1_1vtk_model_callback" ],
+        [ "vtkRefreshCallback", "classpygraver_1_1render_1_1vtk_refresh_callback.html", "classpygraver_1_1render_1_1vtk_refresh_callback" ],
+        [ "vtkTimerCallback", "classpygraver_1_1render_1_1vtk_timer_callback.html", "classpygraver_1_1render_1_1vtk_timer_callback" ],
+        [ "vtkWidgetFader", "classpygraver_1_1render_1_1vtk_widget_fader.html", "classpygraver_1_1render_1_1vtk_widget_fader" ],
+        [ "Wire", "classpygraver_1_1render_1_1_wire.html", "classpygraver_1_1render_1_1_wire" ],
+        [ "WireCollection", "classpygraver_1_1render_1_1_wire_collection.html", "classpygraver_1_1render_1_1_wire_collection" ]
+      ] ],
+      [ "svg", "namespacepygraver_1_1svg.html", [
+        [ "Arc", "classpygraver_1_1svg_1_1_arc.html", "classpygraver_1_1svg_1_1_arc" ],
+        [ "Bezier3", "classpygraver_1_1svg_1_1_bezier3.html", "classpygraver_1_1svg_1_1_bezier3" ],
+        [ "Ellipse", "classpygraver_1_1svg_1_1_ellipse.html", "classpygraver_1_1svg_1_1_ellipse" ],
+        [ "File", "classpygraver_1_1svg_1_1_file.html", "classpygraver_1_1svg_1_1_file" ],
+        [ "Line", "classpygraver_1_1svg_1_1_line.html", "classpygraver_1_1svg_1_1_line" ],
+        [ "Path", "classpygraver_1_1svg_1_1_path.html", "classpygraver_1_1svg_1_1_path" ],
+        [ "Rectangle", "classpygraver_1_1svg_1_1_rectangle.html", "classpygraver_1_1svg_1_1_rectangle" ],
+        [ "Segment", "classpygraver_1_1svg_1_1_segment.html", "classpygraver_1_1svg_1_1_segment" ],
+        [ "Shape", "classpygraver_1_1svg_1_1_shape.html", "classpygraver_1_1svg_1_1_shape" ]
+      ] ],
+      [ "types", "namespacepygraver_1_1types.html", [
+        [ "Path", "classpygraver_1_1types_1_1_path.html", "classpygraver_1_1types_1_1_path" ],
+        [ "PathGroup", "classpygraver_1_1types_1_1_path_group.html", "classpygraver_1_1types_1_1_path_group" ],
+        [ "Point", "classpygraver_1_1types_1_1_point.html", "classpygraver_1_1types_1_1_point" ],
+        [ "Surface", "classpygraver_1_1types_1_1_surface.html", "classpygraver_1_1types_1_1_surface" ]
+      ] ]
+    ] ]
+];
